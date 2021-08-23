9ca46de2dbdfb6553faf17bb3abc58c9ecea4e8d
Ben Follington <5009316+bfollington@users.noreply.github.com>
2021-08-23 13:02:44 +1000

    Multicast mouse streams

--- src/useInput/mouseStream.ts
+++ src/useInput/mouseStream.ts
@@ -1,13 +1,20 @@
 import React from "react"
 import { fromEvent, identity, noop } from "rxjs"
-import { map, pairwise, sampleTime, throttle } from "rxjs/operators"
+import { map, pairwise, sampleTime, share, throttle } from "rxjs/operators"
 
-export const mousemove$ = fromEvent<MouseEvent>(document, "mousemove").pipe(map((ev: MouseEvent) => [ev.clientX, ev.clientY]))
+export const mousemove$ = fromEvent<MouseEvent>(document, "mousemove").pipe(
+  map((ev: MouseEvent) => [ev.clientX, ev.clientY]),
+  share(),
+)
 export const mousemovedelta$ = mousemove$.pipe(
   pairwise(),
   map(([[ax, ay], [bx, by]]) => [bx - ax, by - ay]),
+  share(),
 )
-export const mousemovenormalised$ = mousemove$.pipe(map(([x, y]) => [x / document.documentElement.clientWidth, y / document.documentElement.clientHeight]))
+export const mousemovenormalised$ = mousemove$.pipe(
+  map(([x, y]) => [x / document.documentElement.clientWidth, y / document.documentElement.clientHeight]),
+  share(),
+)
 
 export function useMouseMove(sink: (p: number[]) => void, throttleMs?: number) {
   React.useEffect(() => {
